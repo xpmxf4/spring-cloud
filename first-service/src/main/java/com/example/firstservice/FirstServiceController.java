0b28c12f6ffa21e47e9a89e4e4bdd7c8edfba0e7
dev-chpark <xpmxf5@gmail.com>
2023-10-11 15:06:23 +0900

    왠지 모를 user-service 삭제

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,22 @@
 package com.example.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +30,12 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi there, This is message from First Service check method()";
+    public String check(HttpServletRequest request) {
+        log.info("=============================================" + "Server port={}", request.getServerPort());
+
+        return String.format(
+                "Hi there, This is message from First Service check method() on PORT %s\n",
+                env.getProperty("local.server.port")
+        );
     }
 }
