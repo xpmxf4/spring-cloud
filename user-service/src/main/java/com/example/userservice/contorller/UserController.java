8c84f578530b3fb282608c959674a4e993666ebc
dev-chpark <xpmxf5@gmail.com>
2023-10-11 20:34:27 +0900

    user-service 기본 틀 작성

---
+++ user-service/src/main/java/com/example/userservice/contorller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.userservice.contorller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+//    private final Environment env;
+    private final Greeting greeting;
+    private final UserService userService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+        ResponseUser result = mapper.map(userDto, ResponseUser.class);
+
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+}
